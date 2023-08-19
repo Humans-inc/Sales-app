9b1900ab7c90aa5ac5ba679710474bbdcb13feca
Hanna Babai <kot.vmeshke.4@gmail.com>
2023-08-19 02:13:00 +0200

    fix: empty telegram username

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,14 +1,15 @@
 import './Input.scss';
 const Input = ({ inputPlaceholder, ...props }) => {
+  let isFull = false;
+  if (inputPlaceholder === 'Telegram' && props.value && props.value.length) {
+    isFull = true;
+  }
   return (
     <div className="input-wrap">
       <div className="input-wrap__placeholder">{inputPlaceholder}</div>
-      <input
-        className={`input ${inputPlaceholder === 'Telegram' ? 'full' : ''}`}
-        {...props}        
-      />
+      <input className={`input ${isFull ? 'full' : ''}`} {...props} />
     </div>
   );
 };
 
-export default Input
+export default Input;
